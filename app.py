dea170f8bb71508baa9af1af5d7e07a4d0865719
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-08 19:15:14 +0300

    style: initial file structure

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'this is the home page'
+    return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return 'this is the about page'
 
 if __name__ == '__main__':
     app.run(debug=True)
