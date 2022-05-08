dd8b12eca6514cfffb00146a4987a3d2666c3ae9
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-08 21:11:46 +0300

    feature: add ca config file to get enviroment variables

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import render_template
 from . import app
 from .public import public
 from . import views
 
 app.register_blueprint(public)
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
 
 
 
